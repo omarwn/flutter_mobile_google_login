c4ae407d2b5b2afcfb63bfac690126341ffca639
tontonanrakyat <tontonanrakyat@gmail.com>
2020-02-10 09:16:20 +0700

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../services/sv_firebase_auth.dart';
+
+class HomeScreen extends StatelessWidget {
+  final String userMail;
+
+  HomeScreen(this.userMail);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Welcome " + userMail),
+            RaisedButton(
+              child: Text("log out"),
+              onPressed: () {
+                SvFirebaseAuth().logout();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
